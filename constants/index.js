636587761cf1a59bedc212d334753621e377f88a
Marija Karavla <107922957+KaravlaMarija@users.noreply.github.com>
2023-07-23 14:24:12 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x63625d43f5a0c80277f6f02ebaca83455f6eb24f";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
